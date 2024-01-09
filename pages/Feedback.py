500ec9c50b67b69eaf67736986e5122ff5c4cf36
poooocket <poooocket@qq.com>
2024-01-09 10:53:32 +0800

    Update Feedback.py

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -3,10 +3,9 @@ import pandas as pd
 from datetime import datetime
 
 # 在侧边栏创建一个表单
-with st.sidebar:
-    with st.form(key='feedback_form'):
-        feedback = st.text_area("Share your feedback:")
-        submit_button = st.form_submit_button(label='Submit')
+with st.form(key='feedback_form'):
+    feedback = st.text_area("Share your feedback:")
+    submit_button = st.form_submit_button(label='Submit')
 
 # 处理反馈提交
 if submit_button and feedback:
@@ -26,6 +25,6 @@ if submit_button and feedback:
 
         # 将数据保存到CSV文件
         feedback_data.to_csv('feedback.csv', index=False)
-        st.sidebar.success("Thank you for your feedback!")
+        st.success("Thank you for your feedback!")
     else:
-        st.sidebar.error("An error occurred while processing the feedback.")
+        st.error("An error occurred while processing the feedback.")
